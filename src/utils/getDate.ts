4c1b113bcb703b212992f1cbb0c9479e1d7a1f5b
alyonium <alyonium2.0@gmail.com>
2024-09-18 10:55:43 +0200

    fix date

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,5 +1,15 @@
 export const getDate = (timestamp: string) => {
   const date = new Date(timestamp);
 
-  return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
+  const getMonth = () => {
+    const actualMonth = date.getMonth() + 1;
+
+    if (actualMonth >= 10) {
+      return actualMonth;
+    } else {
+      return `0${actualMonth}`;
+    }
+  };
+
+  return date.getDate() + '/' + getMonth() + '/' + date.getFullYear();
 };
